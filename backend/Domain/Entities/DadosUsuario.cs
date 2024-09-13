723fc515abcda2c257edcff1406e72eb6ef49131
emmy <104332292+emmillylins@users.noreply.github.com>
2024-09-13 14:30:31 -0300

    Adicionando Domain

---
+++ backend/Domain/Entities/DadosUsuario.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class DadosUsuario
+	{
+		public string Id { get; set; }
+		public string Token { get; set; }
+		public TipoUsuario Tipo { get; set; }
+	}
+}
---
+++ backend/Domain/Entities/Login.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+	public class Login
+	{
+		public string Id { get; set; }
+		public string Password { get; set; }
+	}
+
+	public class Register
+	{
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        public string UserName { get; set; } 
+
+        public TipoUsuario TipoUsuario { get; set; }
+    }
+}
