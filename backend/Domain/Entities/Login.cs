692ff4068fae160d15c8d215785cd98ab2ee8cfc
emmy <104332292+emmillylins@users.noreply.github.com>
2024-09-17 11:38:48 -0300

    atualizando classe

--- backend/Domain/Entities/Login.cs
+++ backend/Domain/Entities/Login.cs
@@ -7,11 +7,4 @@ namespace Domain.Entities
 		public string Email { get; set; }
 		public string Password { get; set; }
 	}
-
-    public class DadosUsuario
-    {
-        public string Id { get; set; }
-        public string Token { get; set; }
-        public TipoUsuario Tipo { get; set; }
-    }
 }
