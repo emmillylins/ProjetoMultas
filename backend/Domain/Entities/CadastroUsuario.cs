e5f6d2f50210ef4cf99448a4de82d7ffe8f0a683
emmy <104332292+emmillylins@users.noreply.github.com>
2024-09-20 14:17:40 -0300

    Add files via upload

---
+++ backend/Domain/Entities/CadastroUsuario.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class CadastroUsuario
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        public TipoUsuario TipoUsuario { get; set; }
+    }
+}
---
+++ backend/Domain/Entities/DadosUsuario.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class DadosUsuario
+	{
+		public string Token { get; set; }
+		public string Username { get; set; }
+		public TipoUsuario TipoUsuario { get; set; }
+	}
+}
