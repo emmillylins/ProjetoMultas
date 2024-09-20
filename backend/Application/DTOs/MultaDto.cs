38318bb3bc7134850a899e90723dc7cdde101ae1
emmy <104332292+emmillylins@users.noreply.github.com>
2024-09-20 14:16:31 -0300

    Add files via upload

---
+++ backend/Application/DTOs/MultaDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.DTOs
+{
+    public class MultaDto
+    {
+        public Guid Id { get; set; }
+        public string NumeroAIT { get; set; }
+        public DateTime DataInfracao { get; set; }
+        public string CodigoInfracao { get; set; }
+        public string DescricaoInfracao { get; set; }
+        public string PlacaVeiculo { get; set; }
+        public bool PermiteEdicao { get; set; }
+    }
+    public class CreateMultaDto
+    {
+        public string NumeroAIT { get; set; }
+        public DateTime DataInfracao { get; set; }
+        public string CodigoInfracao { get; set; }
+        public string DescricaoInfracao { get; set; }
+        public string PlacaVeiculo { get; set; }
+    }
+}
---
+++ backend/Application/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<List<ApplicationUser>> GetUsersAsync();
+        Task<DadosUsuario> Login(Login login);
+        Task Logout(string token);
+        Task<string> Register(CadastroUsuario registerModel);
+    }
+}
